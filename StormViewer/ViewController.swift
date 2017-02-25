a0b4a1dc2aa1e7467e44716f2aa8f0b17f39b333
Mark Trance <trance@Marks-iMac.local>
2017-02-25 18:29:37 -0500

    Initial Commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Mark Trance on 2/25/17.
+//  Copyright © 2017 Mark Trance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
