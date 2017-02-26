918f2bc0aade460d6ada4a9859f91f0548f578f7
Mark Trance <trance@Marks-iMac.local>
2017-02-26 12:40:51 -0500

    Final Tweaks

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Storm Viewer"
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
